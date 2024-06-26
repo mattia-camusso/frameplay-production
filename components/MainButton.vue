591a87c812ce713cdbcfa43019e97486cf909d71
mattia.camusso@gmail.com <mattia.camusso@gmail.com>
2024-06-26 10:19:30 +0200

    main not found

---
+++ components/MainButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <a :href="props.link">
+    <button
+      :class="width ? width : 'w-full'"
+      class="relative hover:text-c-2 font-semibold py-3 px-6 after:absolute after:h-1 after:hover:h-[200%] transition-all duration-500 hover:transition-all hover:duration-500 after:transition-all after:duration-500 after:hover:transition-all after:hover:duration-500 overflow-hidden z-20 after:z-[-20] after:bg-c-1 after:w-full after:bottom-0 after:left-0 text-c-1"
+    >
+      <slot />
+    </button>
+  </a>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  width: String,
+  link: String,
+});
+</script>
+
+<style></style>
--- pages/notfound.vue
+++ pages/notfound.vue
@@ -1,5 +1,32 @@
-<template>NOT FOUND</template>
+<template>
+  <div class="not-found">
+    <div
+      class="h-full w-full flex flex-col items-center justify-between pt-[16vh] pb-[8vh]"
+    >
+      <img
+        src="../assets/main-logo.svg"
+        class="max-w-[90vw]"
+        alt="Frameplay Production Logo Svg"
+      />
+      <div class="flex flex-col items-center">
+        <h1 class="text-style-1 leading-relaxed font-semibold text-c-1 pb-6">
+          PAGINA NON TROVATA
+        </h1>
+        <MainButton link="/">TORNA ALLA HOME</MainButton>
+      </div>
+    </div>
+  </div>
+</template>
 
-<script></script>
-
-<style></style>
+<style scoped>
+.not-found {
+  height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  background-position: 90% center;
+  background-repeat: no-repeat;
+  background-image: url("../assets/hero.png");
+  border-image: fill 1
+    linear-gradient(var(--color-overlay), var(--color-overlay));
+}
+</style>
