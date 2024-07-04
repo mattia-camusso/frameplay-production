fab6194e3fca0182f6413ea2dffbb89e8ae423b0
mattia.camusso@gmail.com <mattia.camusso@gmail.com>
2024-07-04 18:36:03 +0200

    fix header temp

--- components/en/HeaderHome.vue
+++ components/en/HeaderHome.vue
@@ -12,7 +12,8 @@
       <a href="/en/about" class="item base-text text-style-3">ABOUT</a>
       <a href="/en/contacts" class="item base-text text-style-3">CONTACTS</a>
     </nav>
-    <a href="/" class="hidden md:block lang base-text text-style-3 mr-8">ENG</a>
+    <!-- 
+    <a href="/" class="hidden md:block lang base-text text-style-3 mr-8">ENG</a> -->
   </header>
 </template>
 
